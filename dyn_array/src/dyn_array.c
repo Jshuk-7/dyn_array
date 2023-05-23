1b48b42263ed4b40f7562bc88246a9f617ed65c3
young_og <jessieshukeireh2001@gmail.com>
2023-05-23 19:06:04 -0400

    added minimalist dynamic array functionality

--- dyn_array/src/dyn_array.c
+++ dyn_array/src/dyn_array.c
@@ -1,6 +1,18 @@
 #define DYN_ARRAY_IMPLEMENTATION
 #include "dyn_array.h"
 
+#include <stdio.h>
+
 int main(void) {
+	DArray* darray = darray_create(sizeof(int));
+	int elem = 10;
+	darray_push_back(darray, sizeof(int), &elem);
+
+	for (size_t i = 0; i < darray->buf_size; i++) {
+		int element = DARRAY_ELEMENT(darray, int, i);
+		printf("%i, ", element);
+	}
+	printf("\n");
+
 	return 0;
 }
--- dyn_array/src/dyn_array.h
+++ dyn_array/src/dyn_array.h
@@ -1,12 +1,96 @@
 #ifndef DYN_ARRAY_H
 #define DYN_ARRAY_H
 
+#include <stdint.h>
+#include <string.h>
 
+#ifndef DA_MALLOC
+#include <stdlib.h>
+#define DA_MALLOC malloc
+#endif
+
+#ifndef DA_REALLOC
+#include <stdlib.h>
+#define DA_REALLOC realloc
+#endif
+
+#ifndef DA_FREE
+#include <stdlib.h>
+#define DA_FREE free
+#endif
+
+#ifndef DA_ASSERT
+#include <assert.h>
+#define DA_ASSERT assert
+#endif
+
+#define DA_DEFAULT_CAPACITY 8
+
+typedef struct
+{
+	uint8_t* buffer;
+	size_t buf_size;
+	size_t element_size;
+	size_t capacity;
+} DArray;
+
+/// @brief Creates a new dynamic array.
+/// @param element_size the size of each element in the array
+/// @return new dynamic array
+DArray* darray_create(size_t element_size);
+
+/// @brief Reallocates the array's buffer to 'new_capacity'.
+/// @param darray the array to modify
+/// @param new_capacity the new capacity of the array
+void darray_realloc(DArray* darray, size_t new_capacity);
+
+/// @brief Pushes a new element to the array. This may involve a reallocation.
+/// @param darray the array to modify
+/// @param element_size the size of each element in the array
+/// @param element a pointer to the element to push to the array
+void darray_push_back(DArray* darray, size_t element_size, const void* element);
+
+#define DARRAY_ELEMENT(darray, type, index) *((type*)(&(darray)->buffer[(darray)->element_size * (index)]))
 
 #endif
 
 #ifdef DYN_ARRAY_IMPLEMENTATION
 
+DArray* darray_create(size_t element_size)
+{
+	DArray* darray = DA_MALLOC(sizeof(DArray));
+	DA_ASSERT(darray != NULL);
 
+	darray->element_size = element_size;
+	darray->buf_size = 0;
+	darray->capacity = DA_DEFAULT_CAPACITY;
+	darray->buffer = DA_MALLOC(darray->capacity * darray->element_size);
+
+	DA_ASSERT(darray->buffer != NULL);
+	return darray;
+}
+
+void darray_realloc(DArray* darray, size_t new_capacity)
+{
+	DA_ASSERT(darray != NULL);
+	DA_ASSERT(darray->buffer != NULL);
+	darray->capacity = new_capacity;
+	darray->buffer = DA_REALLOC(darray->buffer, darray->capacity * darray->element_size);
+	DA_ASSERT(darray->buffer);
+}
+
+void darray_push_back(DArray* darray, size_t element_size, const void* element)
+{
+	DA_ASSERT(darray != NULL);
+	DA_ASSERT(darray->buffer != NULL);
+	DA_ASSERT(darray->element_size == element_size);
+
+	if (darray->buf_size + 1 >= darray->capacity) {
+		darray_realloc(darray, darray->capacity * 2);
+	}
+
+	memcpy(darray->buffer + (darray->buf_size * darray->element_size), element, element_size);
+	darray->buf_size++;
+}
 
 #endif
